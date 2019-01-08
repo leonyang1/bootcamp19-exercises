3bb8978904038ea86c736d35c4d181ee65006e8f
Leon Yang <leonyang@college.harvard.edu>
2019-01-08 16:45:37 -0500

    google api and placeinfo

--- 2_PM/placeinfo.js
+++ 2_PM/placeinfo.js
@@ -2,3 +2,24 @@
 / returns the following string, formatted with the appropriate values:
 
 'INPUTTED_PLACE is located at coordinates RETURNED_COORDINATES, at the address RETURNED_ADDRESS.' */
+const googleMapsClient = require('./google_api.js');
+
+let id;
+// Geocode an address with a promise
+googleMapsClient.findPlace({ input: process.argv[2], inputtype: 'textquery'}).asPromise()
+    .then((response) => {
+      let id = response.json.candidates[0].place_id;
+      googleMapsClient.place({ placeid: id}).asPromise()
+        .then((response) => {
+            let lat = response.json.result.geometry.location.lat
+            let lng = response.json.result.geometry.location.lng
+            let address = response.json.result.formatted_address
+            console.log(`${process.argv[2]} is located at coordinates latitude ${lat} and longitude ${lng}, at the address ${address}.`)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
